e337bdc9a41cf036700035e17200655deed719dd
Yashraj Chavda <yashrajpchavda@gmail.com>
2018-02-22 18:48:19 +0530

    added the router code

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,49 @@
-var express = require('express');
-var router = express.Router();
+const express = require( "express" );
+const router = express.Router();
+const passport = require( "passport" );
+const Account = require( "./../models/account" );
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get( "/", ( req, res ) => {
+  res.render( "index", { user: req.user } );
+} );
+
+router.get( "/register", ( req, res ) => {
+  res.render( "register", {} );
+} );
+
+router.post( "/register", ( req, res ) => {
+
+  Account.resigter( new Account( { username: req.body.username } ), req.body.password, ( err, account ) => {
+
+    if ( err ) {
+      return res.render( "register", { account: account } );
+    }
+
+    passport.authenticate( "local" )( req, res, () => {
+      res.redirect( "/" );
+    } );
+
+  } );
+
+} );
+
+router.get( "/login", ( req, res ) => {
+  res.render( "login", { user: req.user } );
+} );
+
+router.post( "/login", passport.authenticate( "local" ), ( req, res ) => {
+  res.redirect( "/" );
+} );
+
+router.get( "/logout", ( req, res ) => {
+  req.logout();
+  res.redirect( "/" );
+} );
+
+router.get( "/ping", ( req, res ) => {
+  res.status( 200 ).send( "pong!" );
+} );
 
 module.exports = router;
